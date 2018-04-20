7b770d2715f8d223445e50c917239f1c14edd463
sunriseydy <sunriseydy@gmail.com>
2018-04-20 22:10:36 +0800

    添加注意事项
    在插件设置页面显示注意事项

--- views/settings.php
+++ views/settings.php
@@ -52,6 +52,10 @@
             <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php esc_attr_e('Save Changes', 'pure-highlightjs');?>">
             <input type="hidden" name="formaction" value="update-pure-highlightjs">
         </p>
+		<h2>注意</h2>
+		<p>
+			在使用显示行号 JS 脚本时，和 "<code>&lt/code&gt&lt/pre&gt</code>" 在同一行的代码不会显示<br />因此，要将代码的最后一行和 "<code>&lt/code&gt&lt/pre&gt</code>" 用换行隔开<br />
+		</p> <a href="https://blog.sunriseydy.top/technology/server-blog/wordpress/pure-highlightjs-with-line-number/#toc-3" target="_blank">详细教程点我</a>
     </form>
 	<style>
 		.line-color-setting-li{position: relative;padding-left: 80px}
