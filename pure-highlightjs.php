f376a3559e819f0b4897a7dacb9e2347677848e2
sunriseydy <sunriseydy@gmail.com>
2018-04-21 16:23:59 +0800

    更新 SQL 语句
    更新 SQL 语句，在启用插件时自动执行

--- pure-highlightjs.php
+++ pure-highlightjs.php
@@ -22,7 +22,9 @@ define( 'PURE_HIGHLIGHTJS_DEFAULT_THEME', 'default' );
 register_activation_hook( __FILE__, 'pure_highlightjs_activation' );
 register_deactivation_hook( __FILE__, 'pure_highlightjs_deactivation' );
 
+//在启用插件时自动执行 SQL 语句
 function pure_highlightjs_activation() {
+	add_newline_between_code_and_end();
 }
 
 function pure_highlightjs_deactivation() {
@@ -35,11 +37,12 @@ add_action( 'admin_init', 'pure_highlightjs_admin_init' );
 //将之前文章中的代码结束标志和代码最后一行以 \n 隔开
 function add_newline_between_code_and_end() {
 	global $wpdb;
-	$wpdb->query("UPDATE $wpdb->posts SET post_content = replace( post_content, '</code></pre>', '\n</code></pre>' );");
-	$wpdb->query("UPDATE $wpdb->posts SET post_content = replace( post_content, '\n\n</code></pre>', '\n</code></pre>' );");
-	$wpdb->query("UPDATE $wpdb->posts SET post_content = replace( post_content, '\r\n\r\n</code></pre>', '\r\n</code></pre>' );");
+	$wpdb->query("UPDATE $wpdb->posts SET post_content = replace( post_content, '</code></pre>', '\n</code></pre>' ) WHERE post_status = 'publish' AND post_type IN ('post','page');");
+	$wpdb->query("UPDATE $wpdb->posts SET post_content = replace( post_content, '\n\n</code></pre>', '\n</code></pre>' ) WHERE post_status = 'publish' AND post_type IN ('post','page');");
+	$wpdb->query("UPDATE $wpdb->posts SET post_content = replace( post_content, '\r\n\r\n</code></pre>', '\r\n</code></pre>' ) WHERE post_status = 'publish' AND post_type IN ('post','page');");
 	$wpdb->flush();
 }
+
 function pure_highlightjs_admin_init() {
     static $inited = false;
 
@@ -54,8 +57,6 @@ function pure_highlightjs_admin_init() {
     load_plugin_textdomain( 'pure-highlightjs', false, dirname( plugin_basename(__FILE__) ) . '/languages/' );
 
     pure_highlightjs_update_option();
-	
-	add_newline_between_code_and_end();
 
     $inited = true;
 }
